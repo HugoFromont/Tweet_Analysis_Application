2a69097048b50511420150448835d2f14f33ddec
Hugo FROMONT <hugo.fromont35@gmail.com>
2021-01-19 18:39:45 +0100

    add print

--- launch_get_tweet.py
+++ launch_get_tweet.py
@@ -3,11 +3,11 @@ import os
 import json
 from get_tweet.twitter_api import make_request
 
-#start_date = str(datetime.date.today() - datetime.timedelta(days=1))
-#end_date = str(datetime.date.today())
+start_date = str(datetime.date.today() - datetime.timedelta(days=1))
+end_date = str(datetime.date.today())
 
-start_date = "2021-01-01"
-end_date = "2021-01-12"
+#start_date = "2021-01-11"
+#end_date = "2021-01-12"
 
 # Collect request configuration
 path_conf_json = os.path.normpath(os.path.join(os.getcwd(), "get_tweet", "twitter_api", "config.json"))
@@ -22,6 +22,6 @@ list_subject = config["twitter_token"].keys()
 for subject in list_subject:
     try:
         make_request.request_parameters(subject, start_date, end_date)
+        print("Done : ", subject)
     except:
-        print(subject)
-
+        print("KO : ", subject)
